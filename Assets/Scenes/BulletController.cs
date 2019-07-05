0a07296d4577bbc49ab3c12c869209f9f07b4264
NambaTakato <51807649+NambaTakato@users.noreply.github.com>
2019-07-05 17:08:42 +0900

    1回目

---
+++ Assets/Scenes/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(0, 0.2f, 0);
+
+        if (transform.position.y > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Destroy(coll.gameObject);
+        Destroy(gameObject);
+    }
+}
